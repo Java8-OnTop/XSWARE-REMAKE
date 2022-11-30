09dc851224cb06d839eb4bf2d49dab74188f7717
Java8-OnTop <simpsonb11@g.gsacrd.ab.ca>
2022-11-29 20:31:20 -0700

    haha

---
+++ build/gen/buildconfig/src/main/me/travis/BuildConfig.java
@@ -0,0 +1,12 @@
+package me.travis;
+
+/** DO NOT EDIT. GENERATED CODE */
+public final class BuildConfig
+{
+   private BuildConfig () { /*. no instance */ }
+
+   public static final String VERSION = "0.0.6";
+
+   public static final String NAME = "XSWARE(wp3)";
+
+}
